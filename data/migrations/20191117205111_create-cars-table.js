4b0e8fa5251569b91ccb8d38a1aa6b5bae0dbddd
sage-jordan <sagemjrodan@gmail.com>
2019-11-17 22:08:01 -0500

    fixed db

--- data/migrations/20191117205111_create-cars-table.js
+++ data/migrations/20191117205111_create-cars-table.js
@@ -1,20 +1,12 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('cars', tbl => {
+  return knex.schema.createTable('cars', function(tbl){
     tbl.increments();
-    //REQ:
-    //VIN
     tbl.integer('VIN').unique().notNullable();
-    //MAKE
     tbl.string('MAKE').notNullable();
-    //MODEL
     tbl.string('MODEL').notNullable();
-    //MILEAGE
     tbl.integer('MILEAGE').notNullable();
-    //NOT REQ:
-    //TRANSMISSION TYPE
     tbl.string('TransmissionType');
-    //TITLE STATUS
     tbl.string('TitleStatus');
   });
 };
