5312c3cc61f58628486c0668aa51fa112fc31150
sage-jordan <sagemjrodan@gmail.com>
2019-11-17 21:48:56 -0500

    working on seed err

--- data/migrations/20191117205111_create-cars-table.js
+++ data/migrations/20191117205111_create-cars-table.js
@@ -1,24 +1,24 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('cars'), tbl => {
-      tbl.increments();
-      //REQ:
-      //VIN
-      tbl.integer('VIN').unique().notNullable();
-      //MAKE
-      tbl.string('MAKE').notNullable();
-      //MODEL
-      tbl.string('MODEL').notNullable();
-      //MILEAGE
-      tbl.integer('MILEAGE').notNullable();
-      //NOT REQ:
-      //TRANSMISSION TYPE
-      tbl.string('TransmissionType');
-      //TITLE STATUS
-      tbl.string('TitleStatus');
+    tbl.increments();
+    //REQ:
+    //VIN
+    tbl.integer('VIN').unique().notNullable();
+    //MAKE
+    tbl.string('MAKE').notNullable();
+    //MODEL
+    tbl.string('MODEL').notNullable();
+    //MILEAGE
+    tbl.integer('MILEAGE').notNullable();
+    //NOT REQ:
+    //TRANSMISSION TYPE
+    tbl.string('TransmissionType');
+    //TITLE STATUS
+    tbl.string('TitleStatus');
   }
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('cars');
+  return knex.schema.dropTableIfExists('cars');
 };
